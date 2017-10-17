d7437fdd3c050c76171be9f775419bfaa03e9d9d
Tim Evoy <tievo17@gmail.com>
2017-10-16 20:26:56 -0700

    Add view templates.

--- app/app_server/controllers/main.js
+++
@@ -1,7 +0,0 @@
-var index = function(req, res) {
-    res.render('index', { title: 'Express' });
-};
-
-module.exports = {
-    index
-};
--- app/app_server/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var mainController = require('../controllers/main');
-
-/* GET home page. */
-router.get('/', mainController.index);
-
-module.exports = router;
