b9d278588ac4b7e2454acf3a74808099f8aa0fd1
Tim Evoy <tievo17@gmail.com>
2017-11-21 10:41:58 -0800

    Fix to movieSchema compiling into model.

--- app/app_server/models/movie_schema.js
+++ app/app_server/models/movie_schema.js
@@ -1,4 +1,4 @@
-const mongoose = require('Mongoose');
+const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
   film: {
@@ -10,7 +10,6 @@ const movieSchema = new mongoose.Schema({
   year: {
     type: String,
     required: true,
-    enum: ["1942", "1975", "1971", "1939", "1972", "1968", "1959", "1957", "1928"]
   },
   summary: {
     type: String,
@@ -19,8 +18,6 @@ const movieSchema = new mongoose.Schema({
   poster: {
     type: String,
     required: true,
-    enum: ["/images/mp1.jpg", "/images/mp2.jpg", "/images/mp3.jpg", "/images/mp4.jpg", "/images/mp5.jpg",
-          "/images/mp6.jpg", "/images/mp11.jpg", "/images/mp12.jpg", "/images/mp9.jpg"]
   },
   days: [String],
   showTimes: [String]
