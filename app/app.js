9200bcdc0f317546bf093fd6b261e277fb0910bf
Tim Evoy <tievo17@gmail.com>
2017-11-27 21:24:27 -0800

    Setup API for project data.

--- app/app.js
+++ app/app.js
@@ -7,11 +7,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-require('./app_server/models/db');  /* This line executes code in the db.js file */
-const appRouter = require('./app_server/routes/app_router');
+require('./app_server/models/db');  // Executes code in the db.js file
+
+const appRouter = require('./app_server/routes/app_router'); // Router to handle all Application page Requests (Lab 6 Task 4 Part 3)
+const apiRouter = require('./app_api/routes/api_router');  // Router to handle all API Requests (Lab 11 Task 2)
+
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
@@ -23,22 +26,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', appRouter);
+app.use('/', appRouter); // Route every page Request of the Application (Lab 6 Task 4 Part 3)
+app.use('/api', apiRouter); // This directs the API Request for Express to the API Router (Lab 11 Task 2 Part 3)
 
-// catch 404 and forward to error handler
+
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
---
+++ app/app_api/routes/api_router.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Sets up as an Express Router (Lab 11 Task 2 Part 5)
+const express = require('express');
+const router = express.Router();
+
+// Require Controllers that will handle all the API Requests, separating routing code from routing logic (Lab 11 Task 2 Part 6)
+const moviesAPIController = require('../controllers/movies-api');
+const movieTimesAPIController = require('../controllers/movie-times-api');
+
+// Defining API Routes and setting up the Controllers (Lab 11 Task 3 Part 1)
+/* -- movies -- */
+router
+    .route('/movies')
+    
