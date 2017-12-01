aeb364540a818fcee08dc0952b8436422cb28e8a
Tim Evoy <tievo17@gmail.com>
2017-12-01 02:46:24 -0800

    Add genres, length, and rating objects to controller.

--- app/app_server/models/movie_schema.js
+++ app/app_server/models/movie_schema.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
+  theatre: {
+    type: String,
+    required: true,
+    enum: ["Theater #1", "Theater #2", "Theater #3",
+           "Theater #4", "Theater #5", "Theater #6",
+           "Theater #7", "Theater #8", "Theater #9"]
+  },
   title: {
     type: String,
     required: true,
-    enum: ["Casablanca", "One Flew Over The Cuckoo's Nest", "A Clockwork Orange", "Wizard Of Oz", "The Godfather",
-          "2001: A Space Odyssey", "Anatomy Of A Murder", "12 Angry Men", "The Circus"]
+    enum: ["Casablanca", "One Flew Over The Cuckoo's Nest", "A Clockwork Orange",
+          "Wizard Of Oz", "The Godfather", "2001: A Space Odyssey",
+          "Anatomy Of A Murder", "12 Angry Men", "The Circus"]
   },
   year: {
     type: String,
@@ -19,8 +27,8 @@ const movieSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  days: [String],
-  showTimes: [String],
+  frequency: [String],
+  showtimes: [String],
   price: [String]
 });
 
